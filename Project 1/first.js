24ae667d38a3d5fa99416b94dee81ddd7dc445b8
KalpanaChamoli <132485977+KalpanaChamoli@users.noreply.github.com>
2024-07-19 20:45:16 +0530

    Add files via upload

---
+++ Project 1/first.js
@@ -0,0 +1,24 @@
+// JavaScript for changing active menu item
+/*const navbar = document.getElementById('navbar');
+const navItems = navbar.querySelectorAll('a');
+
+navItems.forEach(item => {
+    item.addEventListener('click', function() {
+        // Remove any existing active class
+        navItems.forEach(item => {
+            item.classList.remove('active');
+        });
+
+        // Add active class to the current item
+        this.classList.add('active');
+    });
+});
+
+// JavaScript for changing menu style on scroll
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#222';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});*/
